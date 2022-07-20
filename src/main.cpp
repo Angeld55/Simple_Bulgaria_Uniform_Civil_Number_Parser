b893c79a31200cd3c484677d4c09ac141a646004
Angel Dimitriev <achi_d@abv.bg>
2022-07-20 18:20:03 +0300

    Create main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include "Egn.h"
+
+int main()
+{
+    Egn g("6101057509");
+    
+    std::cout << (int)g.getParseStatus() << std::endl;
+    std::cout << g.getYear() << std::endl;
+    std::cout << g.getMonth() << std::endl;
+    std::cout << g.getDay() << std::endl;
+    
+    std::cout << g.getRegion() << std::endl;
+    
+    std::cout << g.getIsMale() << std::endl;
+
+    std::cout << g.getBornBefore() << std::endl;
+    return 0;
+}
